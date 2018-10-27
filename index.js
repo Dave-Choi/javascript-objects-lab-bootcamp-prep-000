c9dcdf0ad99b23b59852f7eb27cbdb3acac85ac0
David Choi <daaave@gmail.com>
2018-10-27 10:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   // return Object.assign(newObject, newValue);
   var newObject = Object.assign({}, object);
   newObject[key] = value;
-  return 2;
+  
   // return newObject;
 }
 
